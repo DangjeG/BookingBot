9a76b82692ef10e83d9ce3d56a735c171b3b7ad4
RustamDvl <rustamdvl@yandex.ru>
2023-07-02 22:05:07 +0500

    работа над парсером сайта ostrovok.ru

--- ostrovok_parser.py
+++ ostrovok_parser.py
@@ -7,7 +7,7 @@ import time
 MAIN_PAGE = "https://ostrovok.ru/"
 
 
-def get_city_url(country, city, date_in, date_out, adults, childrens):
+def get_city_url(country, city):
     driver = webdriver.Chrome()
     driver.get(MAIN_PAGE)
     city_field = driver.find_element(By.CLASS_NAME, "Input-module__control--tqFEn")
@@ -16,18 +16,10 @@ def get_city_url(country, city, date_in, date_out, adults, childrens):
     time.sleep(3)
     city_field.send_keys(Keys.RETURN)
     driver.find_element(By.CLASS_NAME,
-                        "Button-module__button--MR2Ly.Button-module__button_size_m--184Hw."
+                        "Button-module__button--MR2Ly."
+                        "Button-module__button_size_m--184Hw."
                         "Button-module__button_wide--eV274").click()
     url = driver.current_url
-    url = url.split('dates=')[0] + 'dates=' + date_in + "-" + date_out + '&' + '&'.join(
-        url.split('&')[1:])
-
-    if childrens is None:
-        url = url.split('guests=')[0] + 'guests=' + adults + '&' + '&'.join(
-            url.split('&')[1:])
-    else:
-        url = url.split('guests=')[0] + 'guests=' + adults + "and" + childrens + '&' + '&'.join(
-            url.split('&')[1:])
 
     return url
 
@@ -42,8 +34,27 @@ def main():
     adults = "2"
     childrens = "10.15.3"  # указывается возраст ребёнка,
     # если несколько детей то возраста через точку
+    # минимальное 100 рублей, максимум 100.000
+    price = "0-10000"
+    # звёзды 1-5 через точку
+    stars = "1.2.3"
 
-    city_url = get_city_url(country, city, date_in, date_out, adults, childrens)
+    url = get_city_url(country, city, date_in, date_out, adults, childrens)
+
+    url = url.split('dates=')[0] + 'dates=' + date_in + "-" + date_out + '&' + '&'.join(
+        url.split('&')[1:])
+
+    if childrens is None:
+        url = url.split('guests=')[0] + 'guests=' + adults + '&' + '&'.join(
+            url.split('&')[1:])
+    else:
+        url = url.split('guests=')[0] + 'guests=' + adults + "and" + childrens + '&' + '&'.join(
+            url.split('&')[1:])
+
+    city_url = url.replace("price=one", "price=" + price + ".one")
+
+    if stars != "":
+        city_url = city_url + "&stars=" + stars
     print(city_url)
 
 
